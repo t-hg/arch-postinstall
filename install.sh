26ca567064576a7b6833b9d08f805da740b6f7c7
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-11-03 17:12:25 +0100

    remove stuff

--- install.sh
+++ install.sh
@@ -42,11 +42,6 @@ done
 #sudo pacman -S --noconfirm openresolv
 #
 #
-## C development
-#sudo pacman -S --noconfirm base-devel
-#sudo pacman -S --noconfirm clang
-#sudo pacman -S --noconfirm ccls
-#
 ## Go development
 #sudo pacman -S --noconfirm go
 #sudo pacman -S --noconfirm gopls
@@ -65,39 +60,3 @@ done
 #sudo pacman -S --noconfirm curl
 #sudo pacman -S --noconfirm vscode-css-languageserver
 #sudo pacman -S --noconfirm typescript-language-server
-#
-#
-## SSH
-#touch $HOME/.ssh/config
-#touch $HOME/.ssh/id_rsa
-#touch $HOME/.ssh/id_rsa.pub
-#chmod 600 $HOME/.ssh/id_rsa
-#cat > $HOME/.ssh/config <<'EOF'
-#Host *
-#  AddKeysToAgent 1h
-#EOF
-#sudo pacman -S --noconfirm keychain
-#cat >> $HOME/.bashrc <<'EOF'
-#eval "$(keychain --eval --noask --quiet --agents ssh $HOME/.ssh/id_rsa)"
-#EOF
-#
-#
-## Unrelated packages
-#sudo pacman -S --noconfirm bc
-#sudo pacman -S --noconfirm firefox
-#sudo pacman -S --noconfirm fwupd
-#sudo pacman -S --noconfirm gimp
-#sudo pacman -S --noconfirm inkscape
-#sudo pacman -S --noconfirm libreoffice-still
-#sudo pacman -S --noconfirm linux-headers
-#sudo pacman -S --noconfirm linux-lts-headers
-#sudo pacman -S --noconfirm man-db
-#sudo pacman -S --noconfirm man-pages
-#sudo pacman -S --noconfirm renameutils
-#sudo pacman -S --noconfirm ripgrep-all
-#sudo pacman -S --noconfirm rsync
-#sudo pacman -S --noconfirm thunderbird
-#sudo pacman -S --noconfirm tree
-#sudo pacman -S --noconfirm ttf-iosevka-nerd
-#sudo pacman -S --noconfirm wl-clipboard
-#sudo pacman -S --noconfirm xclip
